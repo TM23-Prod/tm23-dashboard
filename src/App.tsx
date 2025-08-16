83326833956980172c2ef5de710334703595aa4a
Jake <jake.hodson-tomokino@techtorium.ac.nz>
2025-08-17 07:20:39 +1200

    Last try before calling it a night for the time being. OR not, i shouldnt concede or I may never return. We shall see if it works.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import Uploads from "./pages/Uploads.tsx";
